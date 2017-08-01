29c12a2a247d11a40888c1476fdf6ad4ee1b460b
Wouter <prive@wjaudio.nl>
2017-08-01 18:28:33 +0200

    moved fix to lib/dist

--- src/atomic/string.js
+++ src/atomic/string.js
@@ -74,7 +74,7 @@ export default class AtomicString extends Atomic {
     /** @type {number} offset */
     this.offset = pad(offset)
     /** @type {string} value */
-    this.value = new TextDecoder("utf-8").decode(new Uint8Array(data));
+    this.value = String.fromCharCode.apply(null, data);
 
     return this.offset
   }
