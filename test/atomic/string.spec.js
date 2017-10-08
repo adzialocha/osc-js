894da7fbdfe05c17a352381ac8c9cecf16107d73
Andreas Dzialocha <andreas@marmorkuchen.net>
2017-10-08 13:21:58 +0200

    Write a test against pack method throwing errors when converting a large string

--- test/atomic/string.spec.js
+++ test/atomic/string.spec.js
@@ -2,9 +2,20 @@ import { expect } from 'chai'
 
 import AtomicString from '../../src/atomic/string'
 
+function generateLongString(length = 500000) {
+  let str = ''
+
+  for (let i = 0; i < length; i += 1) {
+    str += 'a'
+  }
+
+  return str
+}
+
 /** @test {AtomicString} */
 describe('AtomicString', () => {
   const bitArrayHello = [104, 97, 108, 108, 111, 0, 0, 0]
+
   let atomic
 
   before(() => {
@@ -41,6 +52,16 @@ describe('AtomicString', () => {
       expect(JSON.stringify(atomic.pack())).to.equal(
         JSON.stringify(new Int8Array(bitArrayHello))
       )
+    })
+
+    it('converts a long string without throwing RangeError', () => {
+      const longString = generateLongString()
+      const largeAtomic = new AtomicString(longString)
+      const dataView = new DataView(largeAtomic.pack().buffer)
+
+      expect(() => {
+        largeAtomic.unpack(dataView, 0)
+      }).to.not.throw(RangeError)
     })
   })
 })
