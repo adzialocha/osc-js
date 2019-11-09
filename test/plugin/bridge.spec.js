0a5f3f9b4dd76f65e6294f22ff65132e9ba538c6
eliot-akira <me@eliotakira.com>
2019-11-10 00:21:39 +0100

    Add further tests

--- test/plugin/bridge.spec.js
+++ test/plugin/bridge.spec.js
@@ -1,24 +1,55 @@
 import { expect } from 'chai'
 
+import OSC from '../../src/osc'
+import Message from '../../src/message'
+
 import BridgePlugin from '../../src/plugin/bridge'
+import DatagramPlugin from '../../src/plugin/dgram'
+import WebsocketClientPlugin from '../../src/plugin/wsclient'
+
+const wsPort = 9129
+const udpPort = 9130
 
 /** @test {BridgePlugin} */
 describe('BridgePlugin', () => {
   let plugin
+  let osc
+  let oscWsClient
+  let oscUdpClient
 
   before(() => {
     plugin = new BridgePlugin({
       wsServer: {
-        port: 8129,
+        port: wsPort,
       },
       udpServer: {
         host: '127.0.0.1',
+        port: udpPort,
       },
+    })
+
+    osc = new OSC({
+      plugin,
+    })
+
+    oscWsClient = new OSC({
+      plugin: new WebsocketClientPlugin({
+        port: wsPort,
+      })
+    })
+
+    oscUdpClient = new OSC({
+      plugin: new DatagramPlugin({
+        send: {
+          host: '127.0.0.1',
+          port: udpPort,
+        }
+      })
     })
   })
 
   it('merges the given options correctly', () => {
-    expect(plugin.options.wsServer.port).to.be.equals(8129)
+    expect(plugin.options.wsServer.port).to.be.equals(wsPort)
     expect(plugin.options.udpServer.host).to.be.equals('127.0.0.1')
     expect(plugin.options.receiver).to.be.equals('ws')
   })
@@ -27,5 +58,44 @@ describe('BridgePlugin', () => {
     it('returns the initial status', () => {
       expect(plugin.status()).to.be.equals(-1)
     })
+  })
+
+
+  describe('remote address info', () => {
+    it('returns the remote address info', () => new Promise((resolve, reject) => {
+
+      let resolved = false
+
+      const expectedMessage = {
+        offset: 24,
+        address: '/test/path',
+        types: ',ii',
+        args: [122, 554]
+      }
+
+      const expectedRinfo = {
+        address: '127.0.0.1',
+        family: 'ws',
+        port: wsPort,
+        size: 0,
+      }
+
+      osc.open()
+
+      oscWsClient.open()
+      oscUdpClient.open()
+
+      oscUdpClient.on('/test/path', (message, rinfo) => {
+        expect(message).to.deep.equal(expectedMessage)
+        expect(rinfo).to.deep.equal(expectedRinfo)
+
+        resolve()
+        resolved = true
+      })
+
+      oscWsClient.on('open', () => oscWsClient.send(new Message('/test/path', 122, 554)))
+
+      setTimeout(() => !resolved && reject(new Error('Timeout')), 1000)
+    }))
   })
 })
---
+++ test/plugin/ws.spec.js
@@ -0,0 +1,70 @@
+import { expect } from 'chai'
+
+import OSC from '../../src/osc'
+import Message from '../../src/message'
+
+import WebsocketClientPlugin from '../../src/plugin/wsclient'
+import WebsocketServerPlugin from '../../src/plugin/wsserver'
+
+/** @test {WebsocketClientPlugin} */
+describe('WebsocketClient/ServerPlugin', () => {
+  let plugin
+  let osc
+  let oscServer
+
+  before(() => {
+    plugin = new WebsocketClientPlugin({
+      port: 8129,
+      host: '127.0.0.1',
+    })
+
+    osc = new OSC({
+      discardLateMessages: true,
+      plugin,
+    })
+
+    oscServer = new OSC({
+      discardLateMessages: true,
+      plugin: new WebsocketServerPlugin({
+        port: 8129,
+        host: '127.0.0.1',
+      }),
+    })
+  })
+
+  describe('remote address info', () => {
+    it('returns the remote address info', () => new Promise((resolve, reject) => {
+
+      let resolved = false
+
+      const expectedMessage = {
+        offset: 24,
+        address: '/test/path',
+        types: ',ii',
+        args: [122, 554]
+      }
+
+      const expectedRinfo = {
+        address: '127.0.0.1',
+        family: 'wsserver',
+        port: 8129,
+        size: 0,
+      }
+
+      oscServer.open()
+      osc.open()
+
+      oscServer.on('/test/path', (message, rinfo) => {
+        expect(message).to.deep.equal(expectedMessage)
+        expect(rinfo).to.deep.equal(expectedRinfo)
+
+        resolve()
+        resolved = true
+      })
+
+      osc.on('open', () => osc.send(new Message('/test/path', 122, 554)))
+
+      setTimeout(() => !resolved && reject(new Error('Timeout')), 1000)
+    }))
+  })
+})
