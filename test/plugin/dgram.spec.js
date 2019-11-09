206ca12755d8d7e86c17f479d4eb9976587f0822
eliot-akira <me@eliotakira.com>
2019-11-09 23:07:23 +0100

    Add test for Dram remote address info

--- test/plugin/dgram.spec.js
+++ test/plugin/dgram.spec.js
@@ -1,10 +1,14 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
 
+import OSC from '../../src/osc'
 import DatagramPlugin from '../../src/plugin/dgram'
+import Message from '../../src/message'
+
 
 /** @test {DatagramPlugin} */
 describe('DatagramPlugin', () => {
   let plugin
+  let osc
 
   before(() => {
     plugin = new DatagramPlugin({
@@ -13,7 +17,13 @@ describe('DatagramPlugin', () => {
       },
       open: {
         host: '127.0.0.1',
+        port: 8129,
       },
+    })
+
+    osc = new OSC({
+      discardLateMessages: true,
+      plugin,
     })
   })
 
@@ -26,5 +36,45 @@ describe('DatagramPlugin', () => {
     it('returns the initial status', () => {
       expect(plugin.status()).to.be.equals(-1)
     })
+  })
+
+
+  describe('remote address info', () => {
+    it('returns the remote address info', () => new Promise((resolve, reject) => {
+
+      let message
+      let rinfo
+      let resolved = false
+
+      const expectedMessage = {
+        offset: 24,
+        address: '/test/path',
+        types: ',ii',
+        args: [ 122, 554 ]
+      }
+
+      const expectedRinfo = {
+        address: '127.0.0.1',
+        family: 'IPv4',
+        port: 8129,
+        size: 24,
+      }
+
+      osc.open()
+      osc.on('/test/path', (a, b) => {
+        message = a
+        rinfo = b
+
+        expect(message).to.deep.equal(expectedMessage)
+        expect(rinfo).to.deep.equal(expectedRinfo)
+
+        resolve()
+        resolved = true
+      })
+
+      osc.send(new Message('/test/path', 122, 554))
+
+      setTimeout(() => !resolved && reject(), 1000)
+    }))
   })
 })
