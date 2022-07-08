48b7b8f252919ce20d629c2f804b9d396eaedb5b
Jacob Muchow <jacob.muchow@quarkworks.co>
2022-07-08 15:35:39 -0500

    lint

--- src/external/ws.js
+++ src/external/ws.js
@@ -1,18 +1,25 @@
 // This file gets used instead of the `ws` package during rollup builds
 // targeting browser environments.
-let ws
-
-if (typeof WebSocket !== 'undefined') {
-  ws = WebSocket
-} else if (typeof MozWebSocket !== 'undefined') {
-  ws = MozWebSocket
-} else if (typeof global !== 'undefined') {
-  ws = global.WebSocket || global.MozWebSocket
-} else if (typeof window !== 'undefined') {
-  ws = window.WebSocket || window.MozWebSocket
-} else if (typeof self !== 'undefined') {
-  ws = self.WebSocket || self.MozWebSocket
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+function fillWs() {
+  if (typeof WebSocket !== 'undefined') {
+    return WebSocket
+  } else if (typeof MozWebSocket !== 'undefined') {
+    return MozWebSocket
+  } else if (typeof global !== 'undefined') {
+    return global.WebSocket || global.MozWebSocket
+  } else if (typeof window !== 'undefined') {
+    return window.WebSocket || window.MozWebSocket
+  } else if (typeof self !== 'undefined') {
+    return self.WebSocket || self.MozWebSocket
+  }
+  return undefined
 }
+/* eslint-enable no-undef */
+/* eslint-enable no-restricted-globals */
+
+const ws = fillWs()
 
 /**
  * Do not export server for browser environments.
